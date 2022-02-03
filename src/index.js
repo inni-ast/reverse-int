7b833f2bf401551651579f2df11f8982de6786cb
inni-ast <inni_@tut.by>
2022-02-03 08:39:25 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const string = String(Math.abs(n));
+    return Number(string.split("").reverse().join(''));
 }
